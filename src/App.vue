f1ce432d25644d8bb17df638fed36daeaa235139
IngDavidTM <david-1011@hotmail.com>
2024-05-08 09:13:31 -0500

    Add solution

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Vue Comunication Practice</h1>
+  <user-data @user-submitted="addUser"></user-data>
+  <active-user v-for="user in users" :key="user.id" :name="user.name" :age="user.age"></active-user>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        users: []
+      }
+    },
+    methods: {
+      addUser(user) {
+        this.users.push(user);
+      }
+    }
+  }
+</script>
